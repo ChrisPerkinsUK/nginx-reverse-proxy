ccc327efaf2e70872b83bc1b23d3715c759bed72
ChrisPerkinsCap <chrisperkins.cap@gmail.com>
2023-07-29 00:25:39 +0200

    Added the scripts directory with the 40-disable-nginx-default-conf.sh script

---
+++ scripts/40-disable-nginx-default-conf.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Container: NGINX DIR PATH
+NGINX_DIR="/etc/nginx"
+
+# NGINX SERVICE CONFIG DIR
+NGINX_CONFIG_DIR="${NGINX_DIR}/conf.d"
+
+# Default service file enabled
+DEFAULT_ENABLED="${NGINX_CONFIG_DIR}/default.conf"
+
+# Default service file disabled
+DEFAULT_DISABLED="${NGINX_CONFIG_DIR}/default.off"
+
+# Check if the default service configuration is enabled and disable it
+while [ -f "${DEFAULT_ENABLED}" ]; do
+
+    echo "The NGINX default service configuration is enabled and active!"
+    sleep 1;
+
+    mv "${DEFAULT_ENABLED}" "${DEFAULT_DISABLED}"
+    exit 0;
+done
+
+while [ -f "${DEFAULT_DISABLED}" ]; do
+
+    echo "The NGINX default service configuration is disabled and inactive!"
+    exit 0;
+done
+
+exit 0;
